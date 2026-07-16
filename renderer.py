f567432e996b3fae7e8878103278bcd3957bb184
Majd jamal <115942742+Justmajd@users.noreply.github.com>
2026-07-16 03:22:47 +0300

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -7,6 +7,9 @@ def is_odd(num) -> bool:
     return num % 2 != 0
 
 def render(grid: list[list[int]], entry: tuple[int, int], exit_: tuple[int, int]) -> str:
+    CELL_WIDTH = 3  # Adjust this width to make the cells look square in your terminal
+    wall = "█" * CELL_WIDTH
+    space = " " * CELL_WIDTH
 
     height = len(grid)
     width = len(grid[0]) if grid else 0
@@ -14,19 +17,43 @@ def render(grid: list[list[int]], entry: tuple[int, int], exit_: tuple[int, int]
     for row in range((2*height) + 1):
         current_row: list[str] = []
         for column in range((2*width) + 1):
-            x = (column - 1) // 2
-            y = row // 2
             if is_even(row) and is_even(column):
-                current_row.append("█")
+                current_row.append(wall)
             elif is_even(row) and is_odd(column):
-                if grid[y][x] & MazeGenerator.NORTH:
-                    current_row.append("█")
+                x = (column - 1) // 2
+                y_below = row // 2
+                if y_below < height:
+                    if grid[y_below][x] & MazeGenerator.NORTH:
+                        current_row.append(wall)
+                    else:
+                        current_row.append(space)               
                 else:
-                    current_row.append(" ")
+                    if grid[y_below-1][x] & MazeGenerator.SOUTH:
+                        current_row.append(wall)
+                    else:
+                        current_row.append(space)                   
+
             elif is_odd(row) and is_even(column):
-                if grid[y][x] & MazeGenerator.EAST:
-                    current_row.append("█")
+                x_right = column // 2
+                y = (row - 1) // 2
+                if x_right < width:
+                    if grid[y][x_right] & MazeGenerator.WEST:
+                        current_row.append(wall)
+                    else:
+                        current_row.append(space)
                 else:
-                    current_row.append(" ")
+                    if grid[y][x_right-1] & MazeGenerator.EAST:
+                        current_row.append(wall)
+                    else:
+                        current_row.append(space)
             else:
-                current_row.append(" ")
+                current_row.append(space)
+        lines.append("".join(current_row))
+    return "\n".join(lines)
+        
+
+
+result = render([[15,15],[15,15]], (0,0), (1,1))
+print(len(result.split("\n")))
+
+print(result)
